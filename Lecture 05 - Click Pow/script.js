735e5408fedb6677078ff16c480ae374beb67970
zkirsner <zkirsner@umich.edu>
2024-01-25 11:03:39 -0500

    final answer

--- Lecture 05 - Click Pow/script.js
+++ Lecture 05 - Click Pow/script.js
@@ -1,1 +1,12 @@
 // Write your code here
+const zzzImage = document.querySelector(".clickable")
+
+function clickImage() {
+    if(zzzImage) {
+        zzzImage.classList.add("clicked");
+    }
+    setTimeout(()=> {
+        zzzImage.classList.remove("clicked");
+    }, 1000);
+}
+zzzImage.addEventListener("click", clickImage);
