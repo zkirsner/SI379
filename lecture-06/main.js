cf1f6aea0559b2925ff00e477338069440295b7a
zkirsner <zkirsner@umich.edu>
2024-02-01 11:17:53 -0500

    classwork done

--- lecture-06/main.js
+++ lecture-06/main.js
@@ -9,11 +9,40 @@ button.addEventListener("click", ()=> {
     setTimeout(() => {
         button.removeAttribute("disabled");
     }, 1000);
+    const userInputValue = userInput.value;
     if (userInput.value.length > 0) {
         const listItem = document.createElement("li");
-        listItem.innerText = userInput.value;
+        // listItem.innerText = userInput.value;
         document.querySelector("ul").append(listItem);
+        for (const letter of userInputValue) {
+            const spanEl = document.createElement('span');
+            spanEl.classList.add('letter');
+            spanEl.innerText = letter;
+            listItem.append(spanEl);
+        }
         userInput.value = ""; 
         // clears value of input field^
     }
-})
+});
+userInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        score++;
+        button.setAttribute("disabled", "true");
+        setTimeout(() => {
+            button.removeAttribute("disabled");
+        }, 1000);
+        const userInputValue = userInput.value;
+        if (userInputValue.length > 0) {
+            const listItem = document.createElement("li");
+            // listItem.innerText = userInput.value;
+            document.querySelector("ul").append(listItem);
+            for (const letter of userInputValue) {
+                const spanEl = document.createElement('span');
+                spanEl.classList.add('letter');
+                spanEl.innerText = letter;
+                listItem.append(spanEl);
+            }
+            userInput.value = "";
+        }
+    }
+});
